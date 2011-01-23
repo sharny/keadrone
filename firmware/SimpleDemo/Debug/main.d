6583cc45473940e30b2b8b49bf980719962f372c
willypietersz <willypietersz@7b4f3001-d2f7-861e-258b-27fb7cec29f7>
2011-01-23 17:04:32 +0000

    simeldemo of freertos

---
+++ firmware/SimpleDemo/Debug/main.d
@@ -0,0 +1,42 @@
+main.d main.o: ../main.c \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/FreeRTOS.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/projdefs.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\SimpleDemo/FreeRTOSConfig.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/LPC17xx.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/core_cm3.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/portable.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\portable/portmacro.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/mpu_wrappers.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/task.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/list.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/queue.h \
+  ../type.h ../uart.h
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/FreeRTOS.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/projdefs.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\SimpleDemo/FreeRTOSConfig.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/core_cm3.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/portable.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\portable/portmacro.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/mpu_wrappers.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/task.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/list.h:
+
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/queue.h:
+
+../type.h:
+
+../uart.h:
