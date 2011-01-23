597c09d7ce87fd2b8ea8bf98fcf2c9f903a24589
willypietersz <willypietersz@7b4f3001-d2f7-861e-258b-27fb7cec29f7>
2011-01-23 16:49:58 +0000

    old keadrone procesoor removal

--- trunk/firmware/KeaDrone/24F-source/motorCntrl.c
+++
@@ -1,9 +0,0 @@
-/*
- * motorCntrl.c
- *
- *  Created on: 27 apr 2010
- *  Author	: W.N. Pietersz
- *	Website	: www.wnpd.nl
- */
-
-
--- trunk/firmware/KeaDrone/24F-source/servoCntrl.c
+++
@@ -1,9 +0,0 @@
-/*
- * servoCntrl.c
- *
- *  Created on: 27 apr 2010
- *  Author	: W.N. Pietersz
- *	Website	: www.wnpd.nl
- */
-
-
