76760382c76016dfd99acc3e6302e624b12c5dbd
willypietersz <willypietersz@7b4f3001-d2f7-861e-258b-27fb7cec29f7>
2011-01-25 20:49:03 +0000

    first build completion

--- trunk/firmware/SimpleDemo/Debug/HAL_UART/subdir.mk
+++ trunk/firmware/SimpleDemo/Debug/HAL_UART/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 HAL_UART/%.o: ../HAL_UART/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC17xx -D__CODE_RED -D__REDLIB__ -I"C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc" -I"C:\Users\admin\Desktop\Keadrone\SimpleDemo" -I"C:\Users\admin\Desktop\Keadrone\SimpleDemo\src" -I"C:\Users\admin\Desktop\Keadrone\SimpleDemo\HAL_SPI" -I"C:\Users\admin\Desktop\Keadrone\SimpleDemo\HAL_UART" -I"C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include" -I"C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\portable" -O0 -g3 -fsigned-char -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC17xx -D__CODE_RED -D__REDLIB__ -I"C:\Users\admin\Desktop\Keadrone\SimpleDemo" -I"C:\Users\admin\Desktop\Keadrone\SimpleDemo\src" -I"C:\Users\admin\Desktop\Keadrone\SimpleDemo\HAL_SPI" -I"C:\Users\admin\Desktop\Keadrone\SimpleDemo\HAL_UART" -O0 -g3 -fsigned-char -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
