75fe95dde3b66ddd84067755aae9acffe745a319
willypietersz <willypietersz@7b4f3001-d2f7-861e-258b-27fb7cec29f7>
2011-01-30 10:02:25 +0000

    transfer from laptop to pc. untested changes

--- firmware/SimpleDemo/Debug/HAL_SPI/lpc17xx_ssp.d
+++ firmware/SimpleDemo/Debug/HAL_SPI/lpc17xx_ssp.d
@@ -1,18 +1,18 @@
 HAL_SPI/lpc17xx_ssp.d HAL_SPI/lpc17xx_ssp.o: ../HAL_SPI/lpc17xx_ssp.c \
   ../HAL_SPI/lpc17xx_ssp.h \
-  C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/LPC17xx.h \
-  C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/core_cm3.h \
-  C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/LPC17xx.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/core_cm3.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
   ../HAL_SPI/lpc_types.h ../HAL_SPI/lpc17xx_clkpwr.h \
   ../HAL_SPI/lpc17xx_libcfg_default.h
 
 ../HAL_SPI/lpc17xx_ssp.h:
 
-C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/LPC17xx.h:
 
-C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/core_cm3.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/core_cm3.h:
 
-C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
 
 ../HAL_SPI/lpc_types.h:
 
