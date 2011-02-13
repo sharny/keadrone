8f073329d9debd85aed9ccba638eb880bb97f1f2
willypietersz <willypietersz@gmail.com>
2011-02-13 21:22:58 +0000

    latest freertos build

--- firmware/FreeRTOS_Library/Debug/portable/heap_2.d
+++ firmware/FreeRTOS_Library/Debug/portable/heap_2.d
@@ -1,34 +1,34 @@
 portable/heap_2.d portable/heap_2.o: ../portable/heap_2.c \
-  C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/FreeRTOS.h \
-  C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/projdefs.h \
-  C:\Users\admin\Desktop\Keadrone\SimpleDemo/FreeRTOSConfig.h \
-  C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/LPC17xx.h \
-  C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/core_cm3.h \
-  C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
-  C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/portable.h \
-  C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\portable/portmacro.h \
-  C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/mpu_wrappers.h \
-  C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/task.h \
-  C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/list.h
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/FreeRTOS.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/projdefs.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\SimpleDemo/FreeRTOSConfig.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/LPC17xx.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/core_cm3.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/portable.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\portable/portmacro.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/mpu_wrappers.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/task.h \
+  C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/list.h
 
-C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/FreeRTOS.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/FreeRTOS.h:
 
-C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/projdefs.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/projdefs.h:
 
-C:\Users\admin\Desktop\Keadrone\SimpleDemo/FreeRTOSConfig.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\SimpleDemo/FreeRTOSConfig.h:
 
-C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/LPC17xx.h:
 
-C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/core_cm3.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/core_cm3.h:
 
-C:\Users\admin\Desktop\Keadrone\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
 
-C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/portable.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/portable.h:
 
-C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\portable/portmacro.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\portable/portmacro.h:
 
-C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/mpu_wrappers.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/mpu_wrappers.h:
 
-C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/task.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/task.h:
 
-C:\Users\admin\Desktop\Keadrone\FreeRTOS_Library\include/list.h:
+C:\Users\admin\Documents\lpcxpresso_3.6\workspace\FreeRTOS_Library\include/list.h:
