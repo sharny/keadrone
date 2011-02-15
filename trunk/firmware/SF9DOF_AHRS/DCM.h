4ccc201eca562a492684917334cb2fbea6867316
willypietersz <willypietersz@7b4f3001-d2f7-861e-258b-27fb7cec29f7>
2011-02-15 19:43:15 +0000

    some minor changes

---
+++ trunk/firmware/SF9DOF_AHRS/DCM.h
@@ -0,0 +1,18 @@
+/*
+ * DCM.h
+ *
+ *  Created on: 15 feb 2011
+ *      Author: Willem (wnpd.nl)
+ */
+
+#ifndef DCM_H_
+#define DCM_H_
+
+typedef struct
+{
+	float roll;
+	float pitch;
+	float yaw;
+} GYRO_STRUCT;
+
+#endif /* DCM_H_ */
